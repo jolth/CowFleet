3fe5ad4035ae2cf1ef8bcafe10b125f5280ef8d3
jolth <jolth@dev1.(none)>
2012-10-29 19:20:28 -0500

    upgrade

---
+++ README.md
@@ -0,0 +1,4 @@
+Sistema de Gestión para Activos Móviles.
+-----------------------------------------
+
+Autor: Jorge Alonso Toro Hoyos [jolthgs@gmail.com, jolth@esdebian.org]
--- applications/admin.py
+++ applications/admin.py
@@ -12,7 +12,7 @@ from config import DB
 from view import render
 
 urls = (
-  "", "reuser",
+  "", "re",
   "/", "index",
   "/generalview", "generalview",
   "/viewmonitoreo", "viewmonitoreo",
@@ -23,16 +23,18 @@ urls = (
   "/deletegps", "deletegps",
   "/addvehicle", "addvehicle",
   "/listvehicle","listvehicle",
+  "/listvehiclesjson", "listvehiclesjson",
   "/deletevehicle", "deletevehicle",
   "/assignclient", "assignclient",
   "/addclient", "addclient",
   "/listclient", "listclient",
   "/deleteclient", "deleteclient",
+  "/listclientjson", "listclientjson",
   "/adduser", "adduser",
   "/listuser","listuser",
+  "/deleteuser","deleteuser",
   "/listgspjson", "listgspjson",
   "/listingphones", "listingphones",
-  #"/events","events",
   "/listeventjson","listeventjson",
   "/viewmanagevent","viewmanagevent",
   "/managevents","managevents",
@@ -41,7 +43,7 @@ urls = (
   "/updateevent","updateevent",
 )
 
-class reuser:
+class re:
     def GET(self):
         raise web.seeother('/')
 
@@ -54,21 +56,18 @@ class addgps:
     @Sesion
     def GET(self):
         from appForm import formGps
-
         f = formGps() 
         return renderbase_admin.addgps(web.ctx.session, f) 
 
     def POST(self):
         from appForm import formGps
-
         f = formGps() 
         if f.validates():
             try:
-                # Insert an entry into table 'gps'
                 sequence_id = DB.insert('gps', **f.d)
             except: 
                 return renderbase_admin.addgps(web.ctx.session, f, msgerr='El dispositivo %s ya existe' % f.d.name)
-            return renderbase_admin.addgps(web.ctx.session, f, 'El dispositivo %s, se ha guardado con exito!' % f.d.name)
+            return renderbase_admin.addgps(web.ctx.session, f, u'El dispositivo %s, se ha guardado con éxito!' % f.d.name)
         else:
             return renderbase_admin.addgps(web.ctx.session, f, msgerr=u'Los datos no son válidos.')
             
@@ -88,7 +87,6 @@ class deletegps:
     @Sesion
     def GET(self):
         i = web.input(id=None, name=None)
-        #print "deletegps", i.id, i.name
         try:
             DB.delete('gps', where="id=" + i.id)
         except:
@@ -99,20 +97,16 @@ class addclient:
     @Sesion
     def GET(self):
         from appForm import formClient
-
         f = formClient() 
         return renderbase_admin.addclient(web.ctx.session, f) 
 
     def POST(self):
         from appForm import formClient
-
         f = formClient() 
         if f.validates():
             try:
-                # Configure Storage Phones
                 telefonos = {'fijo':f.d.fijo, 'celular':f.d.celular, 'pbx':f.d.pbx, 'fax':f.d.fax}
                 try:
-                    # The config to's name
                     nombres = f.d.nombres.split(' ') 
                     apellidos = f.d.apellidos.split(' ')
                     nombres.append('')
@@ -121,20 +115,18 @@ class addclient:
                     print "Error Inesperado1 /addclient:", sys.exc_info()
                     return renderbase_admin.addclient(web.ctx.session, f, msgerr='Los Nombre o Apellidos no son validos.')
 
-                # Insert an entry into table 'clientes'
                 sequence_id = DB.insert('clientes', documento=f.d.documento, tipo_docu=f.d.tipo_docu, fecha_naci=f.d.fecha_naci, 
                         direccion=f.d.direccion.lower(), ciudad=f.d.ciudad, sexo_id=f.d.sexo_id, email=f.d.email.lower(), 
                         nombre1=nombres[0].lower(), nombre2=nombres[1].lower() or None, 
                         apellido1=apellidos[0].lower(), apellido2=apellidos[1].lower() or None)
 
                 from db import insertPhone
-                # Insert Phones
                 insertPhone(telefonos, client_id=sequence_id)
                 
             except: 
                 print "Error Inesperado2 /addclient:", sys.exc_info()
                 return renderbase_admin.addclient(web.ctx.session, f, msgerr='El Cliente: %s %s, ya existe' % (f.d.nombres, f.d.apellidos))
-            return renderbase_admin.addclient(web.ctx.session, f, 'El Cliente: %s %s, se ha creado con exito!' % (f.d.nombres, f.d.apellidos))
+            return renderbase_admin.addclient(web.ctx.session, f, u'El Cliente: %s %s, se ha creado con éxito!' % (f.d.nombres, f.d.apellidos))
         else:
             return renderbase_admin.addclient(web.ctx.session, f, msgerr=u'Los datos no son válidos.')
 
@@ -148,8 +140,6 @@ class deleteclient:
     @Sesion
     def GET(self):
         i = web.input(id=None)
-        print "Deleteclient", i.id
-        #return i.id
         try:
             DB.delete('clientes', where="id=" + i.id)
         except:
@@ -160,47 +150,50 @@ class adduser:
     @Sesion
     def GET(self):
         from appForm import formUser
-
         f = formUser() 
         return renderbase_admin.adduser(web.ctx.session, f) 
 
     def POST(self):
         from appForm import formUser
-
         f = formUser() 
         if f.validates():
             try:
-                # Insert an entry into table 'usuarios'
                 sequence_id = DB.insert('usuarios', usuario=f.d.usuario, passwd=f.d.passwd, 
                         privilege_id=f.d.privilege_id, activo=f.d.activo, cliente_id=f.d.cliente_id)
             except: 
                 return renderbase_admin.adduser(web.ctx.session, f, msgerr='El usuario %s ya existe' % f.d.usuario)
-            return renderbase_admin.adduser(web.ctx.session, f, 'El usuario %s, se ha creado con exito!' % f.d.usuario)
+            return renderbase_admin.adduser(web.ctx.session, f, u'El usuario %s, se ha creado con éxito!' % f.d.usuario)
         else:
             return renderbase_admin.adduser(web.ctx.session, f, msgerr=u'Los datos no son válidos.')
 
 class listuser:
     @Sesion
     def GET(self):
-        return listuser
+        from db import listUsuarios
+        return renderbase_admin.listuser(web.ctx.session, listUsuarios())
+
+class deleteuser:
+    @Sesion
+    def GET(self):
+        i = web.input(id=None)
+        try:
+            DB.delete('usuarios', where="id=" + i.id)
+        except:
+            print "Error Inesperado /deleteuser:", sys.exc_info()
+        raise web.seeother('/listuser')
 
 class addvehicle:
     @Sesion
     def GET(self):
         from appForm import formVeh
-
         f = formVeh() 
-        #print f.render()
         return renderbase_admin.addvehicle(web.ctx.session, f) 
     
     def POST(self):
         from appForm import formVeh
-
         f = formVeh() 
         if f.validates():
             try:
-                print "DATA:", f.d
-                # Insert an entry into table 'vehiculos'
                 sequence_id = DB.insert('vehiculos', 
                         placa=f.d.placa.lower(), modelo=f.d.modelo, marca_id=f.d.marca_id, gps_id=f.d.gps_id, 
                         cilindraje=f.d.cilindraje or None, carroceria_id=f.d.carroceria_id or None, 
@@ -209,7 +202,7 @@ class addvehicle:
                         linea_id=f.d.linea_id or None, color_id=f.d.color_id or None)
             except: 
                 return renderbase_admin.addvehicle(web.ctx.session, f, msgerr=u'El Vehículo %s ya existe' % f.d.placa)
-            return renderbase_admin.addvehicle(web.ctx.session, f, u'El vehículo %s, se ha creado con exito!' % f.d.placa)
+            return renderbase_admin.addvehicle(web.ctx.session, f, u'El vehículo %s, se ha creado con éxito!' % f.d.placa)
         else:
             return renderbase_admin.addvehicle(web.ctx.session, f, msgerr=u'Los datos no son válidos.')
 
@@ -217,20 +210,31 @@ class listvehicle:
     @Sesion
     def GET(self):
         from db import listingAllVehicle
-        #return listvehicle
         return renderbase_admin.listvehicle(web.ctx.session, listingAllVehicle())
 
 class assignclient:
     @Sesion
     def GET(self):
-        return assignclient
+        from appForm import formAssignclient
+        f = formAssignclient() 
+        return renderbase_admin.assignclient(web.ctx.session, f) 
+
+    def POST(self):
+        from appForm import formAssignclient
+        f = formAssignclient() 
+        if f.validates():
+            try:
+                sequence_id = DB.insert('clientes_vehiculos', **f.d)
+            except: 
+                return renderbase_admin.assignclient(web.ctx.session, f, msgerr=u'No se pudo crear la relación vehículo(%s) a cliente(%s). Porque ya existe!' % (f.d.vehiculo_id, f.d.cliente_id))
+            return renderbase_admin.assignclient(web.ctx.session, f, u'La relación vehículo(%s) a cliente(%s), se ha guardado con éxito!' % (f.d.vehiculo_id, f.d.cliente_id))
+        else:
+            return renderbase_admin.assignclient(web.ctx.session, f, msgerr=u'Los datos no son válidos.')
 
 class deletevehicle:
     @Sesion
     def GET(self):
         i = web.input(id=None, placa=None)
-        #print "Delete Vehicle", i.id, i.placa
-        #return i.id, i.placa
         try:
             DB.delete('vehiculos', where="id=" + i.id)
         except:
@@ -242,16 +246,10 @@ class generalviewjson:
     def GET(self):
         import simplejson as json 
         from db import generalView
-
-        #views = DB.query("""SELECT l.position, v.placa, g.name, l.fecha, 
-        #        l.velocidad, l.altura, l.satelites, l.ubicacion
-        #        FROM vehiculos v, last_positions_gps l, gps g
-        #        WHERE v.gps_id=g.id AND g.id=l.gps_id""")
         web.header('content-Type', 'application/json')
         def dthandler(obj):
             obj.fecha = obj.fecha.strftime("%F %H:%M:%S")
             return obj
-        #return json.dumps([dthandler(row) for row in views])
         return json.dumps([dthandler(row) for row in generalView()])
 
 class generalview:
@@ -270,29 +268,33 @@ class listgspjson:
     def GET(self):
         import simplejson as json 
         from db import listingDropdown
-
         web.header('content-Type', 'application/json')
         return json.dumps(listingDropdown('gps', "id,name", "id DESC"))
+
+class listclientjson:
+    @Sesion
+    def GET(self):
+        import simplejson as json 
+        from db import listingDropdown
+        web.header('content-Type', 'application/json')
+        return json.dumps(listingDropdown('clientes', "id,documento", "id DESC"))
+
+class listvehiclesjson:
+    @Sesion
+    def GET(self):
+        import simplejson as json 
+        from db import listingDropdown
+        web.header('content-Type', 'application/json')
+        return json.dumps(listingDropdown('vehiculos', "id,placa", "id DESC"))
 
 class listingphones:
     @Sesion
     def GET(self):
-        """
-            http://127.0.0.1:8080/admin/listingphones?id=15
-
-            [["7844983", "fijo"], ["3126783452", "celular"]]
-        """
         import simplejson as json
         from db import listingPhones
-
         i = web.input(id=None)
         web.header('content-Type', 'application/json')
         return json.dumps(listingPhones(i.id))
-
-#class events:
-#    @Sesion
-#    def GET(self):
-#        return events
 
 class listeventjson:
     @Sesion
@@ -316,16 +318,9 @@ class managevents:
 class listingclients:
     @Sesion
     def GET(self):
-        """
-            http://127.0.0.1:8080/admin/listingclients?id=5
-
-            [["jorge,alonso,toro,hoyos", "11814584"]]
-        """
         import simplejson as json
         from db import listingClients
-
         i = web.input(id=None)
-        #print "ID VEHICLE:", i.id
         web.header('content-Type', 'application/json')
         return json.dumps(listingClients(i.id))
     
@@ -333,8 +328,6 @@ class deleteevent:
     @Sesion
     def GET(self):
         i = web.input(id=None)
-        #print "********************************************** Delete Event:", i.id
-        #return i.id
         try:
             DB.delete('eventos', where="id=$i.id", vars=locals())
         except:
@@ -345,15 +338,12 @@ class updateevent:
     @Sesion
     def GET(self):
         i = web.input(id=None)
-        #print "****************************** Update Event:", i.id
         try:
             DB.update('eventos', where='id=$i.id', admin_state='t', vars=locals())
         except:
             print "Error Inesperado /updateevent:", sys.exc_info()    
-        #return updateevent
         raise web.seeother('/managevents')
 
 
 
-# App:
 app_admin = web.application(urls, locals())
